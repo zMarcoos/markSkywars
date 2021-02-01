ad09767c02b8f05079109c5cfc7af794ef015c00
zMarcoos <59182008+zMarcoos@users.noreply.github.com>
2021-02-01 14:44:40 -0300

    Delete src/main/java/marcos/devsoftware/skywars directory

--- src/main/java/marcos/devsoftware/skywars/commands/SkywarsModel.java
+++
@@ -1,8 +0,0 @@
-package marcos.devsoftware.skywars.commands;
-
-import org.bukkit.entity.Player;
-
-public abstract class SkywarsModel {
-
-    public abstract void execute(Player player, String[] args);
-}
